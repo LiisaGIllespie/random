d7906fd74b6cc063e596c678aae33866c02fb88c
Елизавета Мартиросова <eisbadesportler@gmail.com>
2024-02-11 19:23:43 +0300

    initial commit

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int funk2(int i, int j)
+{
+	return i * j;
+}
+
 int main()
 {
 	cout << "hey!" << endl;
+	cout << funk2(4, 10) << endl;
 
 	return 0;
 }
